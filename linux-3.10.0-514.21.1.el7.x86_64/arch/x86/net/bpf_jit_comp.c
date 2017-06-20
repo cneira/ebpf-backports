c39199017042631d6e1fb932bbc87062f8b5749a
cnb <cneirabustos@gmail.com>
2017-06-20 08:52:27 -0400

    fix bjit_free

--- linux-3.10.0-514.21.1.el7.x86_64/arch/x86/net/bpf_jit_comp.c
+++ linux-3.10.0-514.21.1.el7.x86_64/arch/x86/net/bpf_jit_comp.c
@@ -759,10 +759,11 @@ static void jit_free_defer(struct work_struct *arg)
  */
 void bpf_jit_free(struct sk_filter *fp)
 {
-	if (fp->bpf_func != sk_run_filter) {
-		struct work_struct *work = (struct work_struct *)fp->bpf_func;
 
-		INIT_WORK(work, jit_free_defer);
-		schedule_work(work);
+	if (fp->jited) {
+		INIT_WORK(&fp->work, bpf_jit_free_deferred);
+		schedule_work(&fp->work);
+	} else {
+		kfree(fp);
 	}
 }
